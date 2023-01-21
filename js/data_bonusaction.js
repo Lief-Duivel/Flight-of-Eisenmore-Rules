9e0b3382d6778ecc02f2edef72848d8760a97f98
Lief-Duivel <123140832+Lief-Duivel@users.noreply.github.com>
2023-01-20 22:37:03 -0800

    Update data_bonusaction.js

--- js/data_bonusaction.js
+++ js/data_bonusaction.js
@@ -1,10 +1,10 @@
 data_bonusaction = [
     {
-        title: "Offhand Attack",
-        icon: "crossed-swords",
+        title: "Secondary Attack",
+        icon: "offhand",
         subtitle: "Use with the Attack action",
         description: "Attack with your off hand",
-        reference: "PHB, pgs. 192,194-195.",
+        reference: "Bonus Action - Combat",
         bullets: [
             "Only usable if you take the <i>Attack</i> action and attack with a light melee weapon that you're holding in one hand.",
             "Perform a single attack with a different light melee weapon that you're holding in the other hand.",
@@ -14,18 +14,18 @@ data_bonusaction = [
     },
     {
         title: "Cast a spell",
-        icon: "magic-swirl",
+        icon: "spell2",
         subtitle: "Cast time of 1 bonus action",
         description: "Cast a spell with a casting time of 1 bonus action",
-        reference: "PHB, pg. 192.",
+        reference: "Terminology - Spells",
         bullets: [
-            "You can't cast a spell with you action and a different spell with your bonus action in the same turn, except if the action is used to cast a cantrip.",
+            "You can't cast more than two spells in this fashion, only allowing 1 action and 1 bonus action spell per turn.",
             "For further details, see the <i>Cast a spell</i> action."
         ]
     },
     {
         title: "Use class feature",
-        icon: "embrassed-energy",
+        icon: "class2",
         subtitle: "Some features use bonus actions",
         description: "Use a racial or class feature that uses a bonus action",
         reference: "See class page for more information.",
