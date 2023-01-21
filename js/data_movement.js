5cca1c31328a07b3ce6e7e8c48387a7fb3c02302
Lief-Duivel <123140832+Lief-Duivel@users.noreply.github.com>
2023-01-21 01:16:36 -0800

    Update data_movement.js

--- js/data_movement.js
+++ js/data_movement.js
@@ -1,12 +1,11 @@
 data_movement = [
     {
         title: "Move",
-        icon: "run",
-        subtitle: "Cost: 5ft per 5ft",
-        description: "Movement cost: 5ft per 5ft moved",
-        reference: "PHB, pg. 190.",
+        icon: "move",
+        subtitle: "Cost: 5ft per tile",
+        description: "Movement cost: 5ft per tile moved",
+        reference: "Movement",
         bullets: [
-            "If you have more than one speed, such as your walking speed and a flying speed, you can switch back and forth between your speeds during your move. Whenever you switch, subtract the distance you've already moved from the new speed.",
             "You can move through a nonhostile creature's space.",
             "You can move through a hostile creature's space only if the creature is at least two sizes larger or smaller than you.",
             "Another creature's space is difficult terrain for you.",
@@ -15,30 +14,30 @@ data_movement = [
     },
     {
         title: "Climb",
-        icon: "crags",
-        subtitle: "Cost: 10ft per 5ft",
-        description: "Movement cost: 10ft per 5ft climbed",
-        reference: "PHB, pg. 182.",
+        icon: "climb",
+        subtitle: "Cost: 10ft per tile",
+        description: "Movement cost: 10ft per tile climbed",
+        reference: "Movement",
         bullets: [
-            "May involve a Strength (Athletics) check if the climb is difficult"
+            "May involve a <i>Might</i> check if the climb is difficult"
         ]
     },
     {
         title: "Swim",
-        icon: "at-sea",
-        subtitle: "Cost: 10ft per 5ft",
-        description: "Movement cost: 10ft per 5ft swum",
-        reference: "PHB, pg. 182.",
+        icon: "swim",
+        subtitle: "Cost: 10ft per tile",
+        description: "Movement cost: 10ft per tile swum",
+        reference: "Movement",
         bullets: [
-            "May involve a Strength (Athletics) check if the swim is difficult"
+            "May involve a <i>Might</i> check if the swim is difficult"
         ]
     },
     {
         title: "Drop prone",
-        icon: "falling",
+        icon: "prone",
         subtitle: "Cost: 0ft",
         description: "Movement cost: 0ft (free)",
-        reference: "PHB, pgs. 190-191,292.",
+        reference: "Movement - Condition",
         bullets: [
             "You can drop prone without using any of your speed",
             "To move while prone, you must crawl or use magic such as teleportation",
@@ -48,74 +47,59 @@ data_movement = [
     {
         title: "Crawl",
         icon: "crawl",
-        subtitle: "Cost: 10ft per 5ft",
-        description: "Movement cost: 10ft per 5ft crawled",
-        reference: "PHB, pg. 182.",
+        subtitle: "Cost: 10ft per tile",
+        description: "Movement cost: 10ft per tile crawled",
+        reference: "Movement",
         bullets: [
 
         ]
     },
     {
         title: "Stand up",
-        icon: "strong",
+        icon: "stand",
         subtitle: "Cost: half movement speed",
         description: "Movement cost: half of your speed",
-        reference: "PHB, pg. 190-191.",
+        reference: "Movement",
         bullets: [
             "You can't stand up if you don't have enough movement left or if your speed is 0"
         ]
     },
     {
-        title: "High jump",
-        icon: "wingfoot",
-        subtitle: "Cost: 5ft per 5ft",
-        description: "Movement cost: 5ft per 5ft jumped",
-        
-        reference: "PHB, pg. 182.",
+        title: "Jump",
+        icon: "jump",
+        subtitle: "Cost: 5ft per tile",
+        description: "Movement cost: 5ft per tile jumped",
+        reference: "Movement",
         bullets: [
-            "You leap into the air a number of feet equal to <b>3 + your Strength modifier</b> if you move at least 10 feet on foot immediately before the jump.",
-            "When you make a standing high jump, you can jump only half that distance.",
-            "You can extend your arms half your height above yourself during the jump.",
-            "In some circumstances, your DM might allow you to make a Strength (Athletics) check to jump higher than you normally can."
-        ]
-    },
-    {
-        title: "Long jump",
-        icon: "wingfoot",
-        subtitle: "Cost: 5ft per 5ft",
-        description: "Movement cost: 5ft per 5ft jumped",
-        reference: "PHB, pg. 182.",
-        bullets: [
-            "You cover a number of feet up to your <b>Strength score</b> if you move at least 10 feet on foot immediately before the jump.",
-            "When you make a standing long jump, you can leap only half that distance",
-            "May involve a DC 10 Strength (Athletics) check to clear a low obstacle (no taller than a quarter of the jump's distance). You hit the obstacle on a failed check.",
-            "May involve a DC 10 Dexterity (Acrobatics) check to land on your feet in difficult terrain. You land prone on a failed check."
+            "You cover a nnumber of feet up to your <i>Power Score</i>, as long as you move at least 10ft immediately before the jump.",
+            "When you make a standing jump, you can jump only half that distance.",
+            "May involve a <i>Mobility</i> check to clear an obstacle, no taller than a quarter the jump's distance. You hit the object on a failed roll.",
         ]
     },
     {
         title: "Improvise",
-        icon: "juggler",
+        icon: "improvise",
         subtitle: "Any stunt not on this list",
         description: "Perform any movement or stunt you can imagine",
         bullets: [
-            "When you describe a kind of movement not detailed elsewhere in the rules, the DM tells you whether it is possible and what kind of roll you need to make, if any, to determine success or failure."
+            "When you describe a kind of movement not detailed elsewhere in the rules, the GM tells you whether it is possible and what kind of roll you need to make, if any, to determine success or failure."
         ]
     },
     {
         title: "Difficult terrain",
-        icon: "stone-pile",
-        subtitle: "Cost modifier: +5ft per 5ft",
-        reference: "PHB, pg. 182.",
-        description: "Moving in difficult terrain costs an additional 5ft per 5ft of movement",
+        icon: "terraine",
+        subtitle: "Cost modifier: double movement per tile",
+        reference: "Movement",
+        description: "Moving in difficult terrain doubles the cost per tile",
         bullets: [
         ]
     },
     {
         title: "Grapple move",
-        icon: "grab",
+        icon: "grapplemove",
         subtitle: "Modifier: speed halved",
         description: "Drag or carry the grappled creature with you",
-        reference: "PHB, pg. 195.",
+        reference: "Movement",
         bullets: [
             "If you move while grappling another creature, your speed is halved, unless the creature is two or more sizes smaller than you.",
             "See the attack action for how to grapple a creature."
