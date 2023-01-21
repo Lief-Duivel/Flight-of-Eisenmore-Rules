b12dcdf0b3d5a6d4647447df987f964ec0af28c2
Lief-Duivel <123140832+Lief-Duivel@users.noreply.github.com>
2023-01-20 23:12:18 -0800

    Update data_action.js

--- js/data_action.js
+++ js/data_action.js
@@ -14,29 +14,15 @@ data_action = [
         ]
     },
     {
-        title: "Grapple",
-        icon: "grapplemove",
-        subtitle: "Special melee attack",
-        description: "Attempt to grab a creature or wrestle with it",
-        reference: "Actions",
+        title: "Assist",
+        icon: "help",
+        subtitle: "Grant an ally advantage",
+        description: "Grant an ally advantage on an ability check or attack",
+        reference: "Action",
         bullets: [
-            "You can use the <i>Attack</i> action to make a special melee attack, a grapple. If you're able to make multiple attacks with the Attack action, this attack replaces one of them.",
-            "The target of your grapple must be no more than one size larger than you, and it must be within your reach.",
-            "Using at least one free hand, you try to seize the target by making a grapple check, a <i>Might</i> check contested by the target's <i>Might</i> or <i>Mobility</i> check (the target chooses the ability to use).",
-            "If you succeed, you subject the target to the grappled condition (its speed is set to 0)."
-        ]
-    },
-    {
-        title: "Shove",
-        icon: "shove",
-        subtitle: "Special melee attack",
-        description: "Shove a creature, either to knock it prone or push it away from you",
-        reference: "Actions",
-        bullets: [
-            "Using the <i>Attack</i> action, you can make a special melee attack to shove a creature. If you're able to make multiple attacks with the Attack action, this attack replaces one of them.",
-            "The target of your shove must be no more than one size larger than you, and it must be within your reach.",
-            "You make a <i>Might</i> check contested by the target's <i>Might</i> or <i>Mobility</i> check (the target chooses the ability to use).",
-            "If you win the contest, you either knock the target prone or push it 5 feet away from you."
+            "The target gains advantage on the next ability check it makes to perform the task you are helping with.",
+            "Alternatively, the target gains advantage on the next attack roll against against a creature within 5 feet of you.",
+            "The advantage lasts until the start of your next turn."
         ]
     },
     {
@@ -53,13 +39,14 @@ data_action = [
         ]
     },
     {
-        title: "Sprint",
-        icon: "sprint",
-        subtitle: "Double movement speed",
-        description: "Gain extra movement for the current turn",
-        reference: "Action - Movement",
+        title: "Defend",
+        icon: "brace",
+        subtitle: "Increase defenses",
+        description: "Focus entirely on blocking attacks",
+        reference: "Terminology - Dodge or Defend",
         bullets: [
-            "The increase equals your speed, after applying any modifiers."
+            "Until the start of your next turn, your AP increases by half, and you make Might resistance rolls with advantage.",
+            "You lose this benefit if you are <i>incapacitated</i>."
         ]
     },
     {
@@ -83,17 +70,6 @@ data_action = [
         ]
     },
     {
-        title: "Defend",
-        icon: "brace",
-        subtitle: "Increase defenses",
-        description: "Focus entirely on blocking attacks",
-        reference: "Terminology - Dodge or Defend",
-        bullets: [
-            "Until the start of your next turn, your AP increases by half, and you make Might resistance rolls with advantage.",
-            "You lose this benefit if you are <i>incapacitated</i>."
-        ]
-    },
-    {
         title: "Escape",
         icon: "grappleescape",
         subtitle: "Escape a grapple",
@@ -104,61 +80,16 @@ data_action = [
         ]
     },
     {
-        title: "Assist",
-        icon: "help",
-        subtitle: "Grant an ally advantage",
-        description: "Grant an ally advantage on an ability check or attack",
-        reference: "Action",
+        title: "Grapple",
+        icon: "grapplemove",
+        subtitle: "Special melee attack",
+        description: "Attempt to grab a creature or wrestle with it",
+        reference: "Actions",
         bullets: [
-            "The target gains advantage on the next ability check it makes to perform the task you are helping with.",
-            "Alternatively, the target gains advantage on the next attack roll against against a creature within 5 feet of you.",
-            "The advantage lasts until the start of your next turn."
-        ]
-    },
-    {
-        title: "Team Sync",
-        icon: "teamsync",
-        subtitle: "Perform an action with two or more allies, simultaniously",
-        description: "Perform an action with two or more allies, simultaniously",
-        reference: "Terminology - Team Action",
-        bullets: [
-            "Perform a communicated action with two or more allies within 30ft of yourself.",
-            "Half of all targets must succeed a Team Roll, performing an syncronous action on success.",
-            "Targets perform the action immediately on your turn, regardless of initiative - this action uses all targets turns for the round."
-        ]
-    },
-    {
-        title: "Team Combo",
-        icon: "teamcombo",
-        subtitle: "Perform an action with two or more allies, in succession or addition to each other",
-        description: "Perform an action with two or more allies, in succession or addition to each other",
-        reference: "Terminology - Team Action",
-        bullets: [
-            "Perform a communicated action with two or more allies within 30ft of yourself.",
-            "Half of all targets must succeed a Team Roll, performing their actions in the order communicated.",
-            "Targets perform the action immediately on your turn, regardless of initiative - this action uses all targets turns for the round."
-        ]
-    },
-    {
-        title: "Use Item",
-        icon: "item",
-        subtitle: "Interact, use special abilities",
-        description: "Interact with a second object or use special object abilities",
-        reference: "Action",
-        bullets: [
-            "You can interact with one item for free during your turn (such as using an inventory item or opening a door). If you want to interact with a second object, use this action.",
-            "When an object requires your action for its use, you also take this action."
-        ]
-    },
-    {
-        title: "Use shield",
-        icon: "shield",
-        subtitle: "Equip or unequip a shield",
-        description: "Equip or unequip a shield",
-        reference: "Action",
-        bullets: [
-            "A shield always takes an action to equip or unequip.",
-            "Armor takes several minutes to equip or unequip."
+            "You can use the <i>Attack</i> action to make a special melee attack, a grapple. If you're able to make multiple attacks with the Attack action, this attack replaces one of them.",
+            "The target of your grapple must be no more than one size larger than you, and it must be within your reach.",
+            "Using at least one free hand, you try to seize the target by making a grapple check, a <i>Might</i> check contested by the target's <i>Might</i> or <i>Mobility</i> check (the target chooses the ability to use).",
+            "If you succeed, you subject the target to the grappled condition (its speed is set to 0)."
         ]
     },
     {
@@ -176,13 +107,13 @@ data_action = [
         ]
     },
     {
-        title: "Search",
-        icon: "search",
-        subtitle: "",
-        description: "Devote your attention to finding something",
+        title: "Improvise",
+        icon: "improvise",
+        subtitle: "Any action not on this list",
+        description: "Perform any action you can imagine",
         reference: "Action",
         bullets: [
-            "Depending on the nature of your search, the GM might have you make a <i>Percetion</i> check or a <i>Reason</i> check."
+            "When you describe an action not detailed elsewhere in the rules, the GM tells you whether that action is possible and what kind of roll you need to make, if any, to determine success or failure."
         ]
     },
     {
@@ -199,6 +130,74 @@ data_action = [
         ]
     },
     {
+        title: "Search",
+        icon: "search",
+        subtitle: "",
+        description: "Devote your attention to finding something",
+        reference: "Action",
+        bullets: [
+            "Depending on the nature of your search, the GM might have you make a <i>Percetion</i> check or a <i>Reason</i> check."
+        ]
+    },
+    {
+        title: "Shove",
+        icon: "shove",
+        subtitle: "Special melee attack",
+        description: "Shove a creature, either to knock it prone or push it away from you",
+        reference: "Actions",
+        bullets: [
+            "Using the <i>Attack</i> action, you can make a special melee attack to shove a creature. If you're able to make multiple attacks with the Attack action, this attack replaces one of them.",
+            "The target of your shove must be no more than one size larger than you, and it must be within your reach.",
+            "You make a <i>Might</i> check contested by the target's <i>Might</i> or <i>Mobility</i> check (the target chooses the ability to use).",
+            "If you win the contest, you either knock the target prone or push it 5 feet away from you."
+        ]
+    },
+    {
+        title: "Sprint",
+        icon: "sprint",
+        subtitle: "Double movement speed",
+        description: "Gain extra movement for the current turn",
+        reference: "Action - Movement",
+        bullets: [
+            "The increase equals your speed, after applying any modifiers."
+        ]
+    },
+    {
+        title: "Team Combo",
+        icon: "teamcombo",
+        subtitle: "Perform an action with two or more allies, in succession or addition to each other",
+        description: "Perform an action with two or more allies, in succession or addition to each other",
+        reference: "Terminology - Team Action",
+        bullets: [
+            "Perform a communicated action with two or more allies within 30ft of yourself.",
+            "Half of all targets must succeed a Team Roll, performing their actions in the order communicated.",
+            "Targets perform the action immediately on your turn, regardless of initiative - this action uses all targets turns for the round."
+        ]
+    },
+    {
+        title: "Team Sync",
+        icon: "teamsync",
+        subtitle: "Perform an action with two or more allies, simultaniously",
+        description: "Perform an action with two or more allies, simultaniously",
+        reference: "Terminology - Team Action",
+        bullets: [
+            "Perform a communicated action with two or more allies within 30ft of yourself.",
+            "Half of all targets must succeed a Team Roll, performing an syncronous action on success.",
+            "Targets perform the action immediately on your turn, regardless of initiative - this action uses all targets turns for the round."
+        ]
+    },
+    {
+        title: "Use Item",
+        icon: "item",
+        subtitle: "Interact, use special abilities",
+        description: "Interact with a second object or use special object abilities",
+        reference: "Action",
+        bullets: [
+            "You can interact with one item for free during your turn (such as using an inventory item or opening a door). If you want to interact with a second object, use this action.",
+            "When an object requires your action for its use, you also take this action."
+        ]
+    },
+    {
         title: "Use class feature",
         icon: "class",
         subtitle: "Some features use actions",
@@ -209,13 +208,14 @@ data_action = [
         ]
     },
     {
-        title: "Improvise",
-        icon: "improvise",
-        subtitle: "Any action not on this list",
-        description: "Perform any action you can imagine",
+        title: "Use shield",
+        icon: "shield",
+        subtitle: "Equip or unequip a shield",
+        description: "Equip or unequip a shield",
         reference: "Action",
         bullets: [
-            "When you describe an action not detailed elsewhere in the rules, the GM tells you whether that action is possible and what kind of roll you need to make, if any, to determine success or failure."
+            "A shield always takes an action to equip or unequip.",
+            "Armor takes several minutes to equip or unequip."
         ]
     }
 ]
