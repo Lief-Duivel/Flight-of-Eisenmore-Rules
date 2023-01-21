e80f5ce9006e49b72bb00a4f4e6c96a617603cb6
Lief-Duivel <123140832+Lief-Duivel@users.noreply.github.com>
2023-01-20 23:43:02 -0800

    Update data_criticalcondition.js

--- js/data_criticalcondition.js
+++ js/data_criticalcondition.js
@@ -1,36 +1,75 @@
 data_criticalcondition = [
     {
-        title: "Offhand Attack",
-        icon: "crossed-swords",
-        subtitle: "Use with the Attack action",
-        description: "Attack with your off hand",
-        reference: "PHB, pgs. 192,194-195.",
+        title: "Crippled",
+        icon: "crippled",
+        subtitle: "You received a crippling blow",
+        description: "You fell to 0 HP and received a crippling blow",
+        reference: "Critical Condition",
         bullets: [
-            "Only usable if you take the <i>Attack</i> action and attack with a light melee weapon that you're holding in one hand.",
-            "Perform a single attack with a different light melee weapon that you're holding in the other hand.",
-            "You don't add your ability modifier to the damage of the bonus attack, unless that modifier is negative.",
-            "If either weapon has the thrown property, you can throw the weapon, instead of making a melee attack with it."
+            "When you receive a crippling blow, you become unable to use the limb, or other extremity, as determined by the GM.",
+            "An affected limb may still be used with a successful <i>Stamina</i> roll, but risks permanently injuring the limb.",
+            "The effect ends if magically treated, or after 5 successful Camps."
         ]
     },
     {
-        title: "Cast a spell",
-        icon: "magic-swirl",
-        subtitle: "Cast time of 1 bonus action",
-        description: "Cast a spell with a casting time of 1 bonus action",
-        reference: "PHB, pg. 192.",
+        title: "Disoriented",
+        icon: "disoriented",
+        subtitle: "You become disoriented",
+        description: "You fell to 0 HP and became disoriented",
+        reference: "Critical Condition",
         bullets: [
-            "You can't cast a spell with you action and a different spell with your bonus action in the same turn, except if the action is used to cast a cantrip.",
-            "For further details, see the <i>Cast a spell</i> action."
+            "When you become disoriented, you make all actions, bonus actions, and reactions with disadvantage.",
+            "While disoriented, you can still take movement, but it will be in a random cardinal direction, as decided by a d4 roll.",
+            "The effect only ends after a successful camp."
         ]
     },
     {
-        title: "Use class feature",
-        icon: "embrassed-energy",
-        subtitle: "Some features use bonus actions",
-        description: "Use a racial or class feature that uses a bonus action",
-        reference: "See class page for more information.",
+        title: "Exhaustion",
+        icon: "exhaustion",
+        subtitle: "You are exhausted",
+        description: "Exhaustion is measured in three levels",
+        reference: "Critical Condition",
         bullets: [
-
+            "<table><tr><th>Level</th><th></th><th></th><th style='text-align:left'>Effect</th></tr><tr><td>1</td><td></td><td></td><td>Disadvantage on skill checks, aim rolls, and resistance rolls</td></tr><tr><td>2</td><td></td><td></td><td>Movement halved</td></tr><tr><td>3</td><td></td><td></td><td>Movement speed reduced to 0</td></tr><tr><td>++</td><td></td><td></td><td>Become unconcious</td></tr></table>",
+            "You suffer the effect of your current level of exhaustion as well as all lower levels.",
+            "Exhaustion levels are gained every two turns you take, starting from the 1st level.",
+            "A successful Camp removes your exhaustion, provided that you have also had some food and drink.",
+        ]
+    },
+    {
+        title: "Injured",
+        icon: "injured",
+        subtitle: "You become injured",
+        description: "You fell to 0 HP and became injured",
+        reference: "Critical Condition",
+        bullets: [
+            "When you become injured, you take on the bleed condition. Actions, bonus actions, reactions, and movement can be taken at the cost of Bleed Damage.",
+            "Actions cost 5 HP, Bonus actions and reactions cost 3, while Movement costs 1 HP per 5ft.",
+            "The effects ends if the wound is sealed or magically treated, and does not end at Camp unless treated."
+        ]
+    },
+    {
+        title: "Perish",
+        icon: "death",
+        subtitle: "You perish",
+        description: "You fell to 0 HP and perished",
+        reference: "Critical Condition",
+        bullets: [
+            "This condition can only be reached if 2 other Critical Conditions have stacked, making Perish the 3rd. Or, you receive damage more than five times your maximum HP.",
+            "Speak with your GM to confirm whether or not you wouud like Perish as an option for your character.",
+        ]
+    },
+    {
+        title: "Unconcious",
+        icon: "unconcious",
+        subtitle: "You fall unconcious",
+        description: "You fall unconcious",
+        reference: "Critical Condition",
+        bullets: [
+            "This condition is gained by passing level 3 Exhaustion; as an alternative to Perish; or if you receive damage more than two times your max HP.",
+            "This condition can be removed by a successful camp, or a successful Waking Roll. Waking Rolls are 1d6, successful rolls 5-6.",
+            "Allies may assist in Waking Rolls, changing the successful roll to 3-6."
+            "While Unconcious you may not take actions, bonus actions, or reactions, and your movement is reduced to 0."
         ]
     }
 ]
