3387a356c1955ebd71a86d0f7be5ebb126a3b54e
Lief-Duivel <123140832+Lief-Duivel@users.noreply.github.com>
2023-02-08 20:36:35 -0800

    Update data_bonusaction.js

--- js/data_bonusaction.js
+++ js/data_bonusaction.js
@@ -4,22 +4,22 @@ data_bonusaction = [
         icon: "offhand",
         subtitle: "Use with the Attack action",
         description: "Attack with your off hand",
-        reference: "Bonus Action - Combat",
+        reference: "Secondary Action - Combat",
         bullets: [
             "Only usable if you take the <i>Attack</i> action and attack with a light melee weapon that you're holding in one hand.",
             "Perform a single attack with a different light melee weapon that you're holding in the other hand.",
-            "You don't add your ability modifier to the damage of the bonus attack, unless that modifier is negative.",
+            "You don't add your ability modifier to the damage of the secondary attack, unless that modifier is negative.",
             "If either weapon has the thrown property, you can throw the weapon, instead of making a melee attack with it."
         ]
     },
     {
         title: "Cast a spell",
         icon: "spell2",
-        subtitle: "Cast time of 1 bonus action",
-        description: "Cast a spell with a casting time of 1 bonus action",
+        subtitle: "Cast time of 1 secondary action",
+        description: "Cast a spell with a casting time of 1 secondary action",
         reference: "Terminology - Spells",
         bullets: [
-            "You can't cast more than two spells in this fashion, only allowing 1 action and 1 bonus action spell per turn.",
+            "You can't cast more than two spells in this fashion, only allowing 1 action and 1 secondary action spell per turn.",
             "For further details, see the <i>Cast a spell</i> action."
         ]
     },
@@ -40,15 +40,15 @@ data_bonusaction = [
         description: "Switch either your main or secondary weapon",
         reference: "Action",
         bullets: [
-            "Only one weapon can be swapped during a bonus action.",
+            "Only one weapon can be swapped during a secondary action.",
             "Weapons of a greater weight or size than the weapon equipped take 1 full action."
         ]
     },
     {
         title: "Use class feature",
         icon: "class2",
-        subtitle: "Some features use bonus actions",
-        description: "Use a racial or class feature that uses a bonus action",
+        subtitle: "Some features use secondary actions",
+        description: "Use a racial or class feature that uses a secondary action",
         reference: "See class page for more information.",
         bullets: [
 
@@ -61,7 +61,7 @@ data_bonusaction = [
         description: "Equip or unequip a shield",
         reference: "Action",
         bullets: [
-            "A shield always takes a bonus action to equip or unequip.",
+            "A shield always takes a secondary action to equip or unequip.",
             "Armor takes several minutes to equip or unequip."
         ]
     }
